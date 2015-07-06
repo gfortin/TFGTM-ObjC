859a09459de493de3cb48675bfe138d2a279f2ed
Ghislain Fortin <ghislain_fortin@natural-solutions.eu>
2015-07-06 23:34:08 +0200

    About web view in Swift

---
+++ tfgtm/AboutViewController.swift
@@ -0,0 +1,35 @@
+//
+//  About.swift
+//  tfgtm
+//
+//  Created by Ghislain Fortin on 06/07/2015.
+//  Copyright (c) 2015 MobileServices. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var aboutWebView: UIWebView!
+    
+    let urlGEP = "http://www.groupe-ecolepratique.com"
+    
+     //     self.window.title = "A propos"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        var tryPage = NSURL(fileURLWithPath:NSBundle.mainBundle().pathForResource("webTFGTM", ofType:"html",inDirectory: "web")!)
+        
+        let reqPage = NSURLRequest(URL: tryPage!)
+        var requestPage = NSURLRequest(URL: tryPage!);
+        aboutWebView.loadRequest(requestPage)
+
+        // let requestURL = NSURL(string:urlGEP)
+        // let request = NSURLRequest(URL: requestURL!)
+        // aboutWebView.loadRequest(request)
+        
+    }
+    
+    
+}
